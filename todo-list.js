97e5bb7cf73bf1ffa9a63da65d4d02b0d21bc397
Mohamed Azarudeen <160878803+azarGH101@users.noreply.github.com>
2024-11-13 17:52:39 +0530

    Update todo-list.js

--- todo-list.js
+++ todo-list.js
@@ -10,17 +10,36 @@ const taskAddMsg = 'Task added Successfully';
 const taskRemMsg = 'Task removed Successfully';
 const taskCompMsg = 'Task Completed Successfully';
 
+const finishedTasks = document.getElementById("finished");
+const totalTasks = document.getElementById("totalTasks");
+const lineAnimate = document.querySelector(".line");
+
+let finishedTask = 0;
+let non_FinishedTask = 0;
+let totalTasksList = 0;
+
 let temp = JSON.parse(localStorage.getItem(localStorageName)) || [];
 
 // MAIN FUNCTION
 function main() {
-    // console.log(temp);
+    // if(input.value === '') {
+    //     addTaskBtn.disabled = true;
+    //     addTaskBtn.cursor = 'not-allowed';
+    // }
+    // else {
+    //     addTaskBtn.disabled = false;
+    // }
     if(temp.length!==0) {
         temp.forEach((val)=>{
-            // console.log(val);
             createTaskElement(val);
         }) ;
     }
+    finishedTasks.innerHTML = finishedTask;
+    totalTasksList = finishedTask+non_FinishedTask;
+    totalTasks.innerHTML = totalTasksList;
+
+    finishTasksAnimation(totalTasksList,finishedTask);
+
     // EDIT TASK NAME
     const editBtns = document.querySelectorAll('.edit');
 
@@ -32,15 +51,66 @@ function main() {
     const deleteBtns = document.querySelectorAll('.delete');
 
     for(let i=0;i<deleteBtns.length;i++) {
-        deleteBtns[i].addEventListener("click",()=>removeTask(i));
+        deleteBtns[i].addEventListener("click",()=>{
+            totalTasksList--;
+            totalTasks.innerHTML = totalTasksList;
+            finishedTask--;
+            finishedTasks.innerHTML = finishedTask;
+            removeTask(i);
+            finishTasksAnimation(totalTasksList,finishedTask);
+            pageReload();
+            // const taskLineArr = document.querySelectorAll('.taskLine');
+            // console.log(taskLineArr);
+        });
     };
 
     // STRIKE THROUGH FINISHED TASKS
     const radioBtns = document.querySelectorAll('.radioBtn');
+    const delBtns = document.querySelectorAll('.delete');
 
-    radioBtns.forEach((radio)=>{
-        radio.addEventListener("click",()=>strikeThrough(radio));
-    });
+    for(let i=0;i<radioBtns.length;i++) {
+        radioBtns[i].addEventListener("click",()=>{
+            finishedTask++;
+            finishedTasks.innerHTML = finishedTask;
+            console.log(finishedTask);
+            strikeTaskAnimation();
+            // finishTasksAnimation(totalTasksList,finishedTask);
+            strikeThrough(i);
+            pageReload();
+        });
+    };
+}
+
+function strikeTaskAnimation() {
+    const animateLines = document.querySelectorAll('.taskLine');
+    console.log(animateLines);
+    for(let i=0;i<totalTasksList;i++) {
+    if(!animateLines[i].classList.contains('tl-Animation')) {
+            animateLines[i].classList.add('tl-Animation');
+            break;
+        }
+    };   
+}
+
+function finishTasksAnimation(total,finish) {
+    const taskLineArr = document.querySelectorAll('.taskLine');
+    // console.log(taskLineArr);
+    if(taskLineArr.length!==0) {
+        taskLineArr.forEach((element)=>{
+            element.remove();
+        });
+    }
+    for(let i=0;i<total;i++) {
+        // CREATE TASK FINISHED ANIMATION LINE
+        const taskLinesss = document.createElement('div');
+        taskLinesss.classList.add('taskLine');
+        taskLinesss.style.width = `${100/totalTasksList}%`;        
+        if(finish>0) {
+            taskLinesss.classList.add('tl-Animation'); 
+            finish--;
+        }
+        lineAnimate.appendChild(taskLinesss);
+    }
 }
 
 // EDILT TASK NAME 
@@ -53,32 +123,41 @@ function editTaskName(pos) {
 }
 // REMOVE TASK FROM LOCAL STORAGE
 function removeTask(pos) {
-    // console.log('Before Delete : ', temp);
     temp.splice(pos,1);
     localStorage.setItem(localStorageName,JSON.stringify(temp)); 
     result.style.color = 'red';
     result.innerHTML = taskRemMsg;
-    // console.log('After Delete : ', temp);
     const parentDiv = document.querySelectorAll('.taskDataDiv');
     parentDiv[pos].remove();
 }
 
 
 // STRIKE THROUGH TASK
-function strikeThrough(selectedBtn) {
-    const radioBtns = document.querySelectorAll('.radioBtn');
+function strikeThrough(pos) {
+    const label = document.querySelectorAll('.taskName');
     const editBtns = document.querySelectorAll('.edit');
-    for(let i=0;i<radioBtns.length;i++) {
-        if(radioBtns[i]===selectedBtn) {
-            const label = radioBtns[i].parentElement;
-            label.style.textDecoration = 'line-through';
-            editBtns[i].disabled = true;
-            result.style.color = 'blue';
-            result.innerHTML = taskCompMsg;
-            changeStatus(i);
-            break;
-        }
-    }
+    const delBtns = document.querySelectorAll('.delete');
+    
+    label[pos].style.textDecoration = 'line-through';
+    // EDIT BUTTON CHANGE
+    editBtns[pos].disabled = true;
+    editBtns[pos].style.cursor = 'not-allowed';
+    editBtns[pos].style.backgroundColor = 'grey';
+    // DELETE BUTTON CHANGED
+    delBtns[pos].disabled = false;
+    delBtns[pos].style.cursor = 'pointer';
+    delBtns[pos].style.backgroundColor = 'rgb(245, 188, 118)';
+
+    result.style.color = 'blue';
+    result.innerHTML = taskCompMsg;
+    
+    changeStatus(pos);
+}
+
+function pageReload() {
+    setTimeout(function() {
+        location.reload();
+      }, 1000); // 2000 milliseconds = 2 seconds      
 }
 
 // CHANGE STATUS OF COMPLETED TASK IN LOCAL STORAGE
@@ -91,14 +170,26 @@ function changeStatus(pos) {
 // ADD TASK TO LOCAL STORAGE 
 addTaskBtn.addEventListener("click",()=>{
     const task = input.value;
-    result.innerHTML = taskAddMsg;
-    input.value='';
+    if(task!=='') {
+        result.innerHTML = taskAddMsg;
+        input.value='';
 
-    // ADD TASK TO LOCAL STORAGE
-    let tempObj = {value:task,status:'Non-Complete'};
-    temp.push(tempObj);
-    localStorage.setItem(localStorageName,JSON.stringify(temp));
-    createTaskElement(tempObj);
+        // ADD TASK TO LOCAL STORAGE
+        let tempObj = {value:task,status:'Non-Complete'};
+        temp.push(tempObj);
+        localStorage.setItem(localStorageName,JSON.stringify(temp));
+        console.log('Finished Task = ',finishedTask);
+        totalTasksList = JSON.parse(localStorage.getItem(localStorageName)).length;
+        createTaskElement(tempObj);
+        
+        finishTasksAnimation(totalTasksList,finishedTask);
+        totalTasks.innerHTML = totalTasksList;        
+        console.log('Total Task = ',totalTasksList);
+        pageReload();
+    }
+    else {
+        result.innerHTML = 'Please Enter a Value';
+    }
 });
 
 
@@ -145,6 +236,15 @@ function createTaskElement(obj) {
         labelElement.style.textDecoration = 'line-through';
         radio.disabled = 'true';
         editBtn.disabled = true;
+        editBtn.style.cursor = 'not-allowed';
+        editBtn.style.backgroundColor = 'grey';
+        finishedTask++;
+    }
+    else {
+        trashBtn.disabled = true;
+        trashBtn.style.cursor = 'not-allowed';
+        trashBtn.style.backgroundColor = 'grey';
+        non_FinishedTask++;
     }
 
     // trashBtn.value = iframe;
